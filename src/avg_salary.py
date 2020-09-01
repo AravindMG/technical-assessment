28b8f19dc7752d969d7efefbfb3d1118be9e6d67
Aravind Madana Gopal <amad295@aucklanduni.ac.nz>
2020-09-01 22:24:50 +1200

    Completed test cases and refactoring

--- src/avg_salary.py
+++ src/avg_salary.py
@@ -7,113 +7,123 @@ def read_json():
         data = json.load(sample_data)
         employees = data['Employees']
         df = json_normalize(employees)
-        print(df.head(3))
         return df
 
 
+# Here we are filtering only the necessary columns needed for the average salary calculation in each region
+# Also we are removing ',' to make the salary column as integer type
 def filter_necessary_cols(df):
     df_filtered_cols = df[['jobTitleName', 'region', 'salary']]
     df_filtered_cols['salary'] = df_filtered_cols['salary'].replace(',', '', regex=True)
     df_filtered_cols['salary'] = df_filtered_cols['salary'].astype('int')
-    # print(df_filtered_cols)
     return df_filtered_cols
 
 
-# In SA region there are no devs
-def filter_dev_CA(df_filtered_cols):
-    CA_dev = df_filtered_cols[
+# In this method we are filtering developer's in CA region
+def filter_dev_ca(df_filtered_cols):
+    ca_dev = df_filtered_cols[
         (df_filtered_cols['jobTitleName'] == 'Developer') & (df_filtered_cols['region'] == 'CA')]
-    # print(CA_dev)
-    return CA_dev
+    return ca_dev
 
 
-def avg_sal_dev_CA(CA_dev):
-    mean_salary = CA_dev['salary'].mean()
-    print(mean_salary)
+# In this method we are calculating the average salary of a developer in CA region by taking a mean of the salary column
+def avg_sal_dev_ca(ca_dev):
+    mean_salary = ca_dev['salary'].mean()
     return "In 'CA' region the average salary of a 'Developer' is " + str(mean_salary) + "\n"
 
 
-def filter_dev_FA(df_filtered_cols):
-    FA_dev = df_filtered_cols[
+# In this method we are filtering developer's in FA region
+def filter_dev_fa(df_filtered_cols):
+    fa_dev = df_filtered_cols[
         (df_filtered_cols['jobTitleName'] == 'Developer') & (df_filtered_cols['region'] == 'FA')]
-    print(FA_dev)
-    return FA_dev
+    return fa_dev
 
 
-def avg_sal_dev_FA(FA_dev):
-    mean_salary = FA_dev['salary'].mean()
+# In this method we are calculating the average salary of a developer in FA region by taking a mean of the salary column
+def avg_sal_dev_fa(fa_dev):
+    mean_salary = fa_dev['salary'].mean()
     return "In 'FA' region the average salary of a 'Developer' is " + str(mean_salary) + "\n"
 
 
-def filter_dev_YA(df_filtered_cols):
-    YA_dev = df_filtered_cols[
+# In this method we are filtering developer's in YA region
+def filter_dev_ya(df_filtered_cols):
+    ya_dev = df_filtered_cols[
         (df_filtered_cols['jobTitleName'] == 'Developer') & (df_filtered_cols['region'] == 'YA')]
-    print(YA_dev)
-    return YA_dev
+    return ya_dev
 
 
-def avg_sal_dev_YA(YA_dev):
-    mean_salary = YA_dev['salary'].mean()
+# In this method we are calculating the average salary of a developer in YA region by taking a mean of the salary column
+def avg_sal_dev_ya(ya_dev):
+    mean_salary = ya_dev['salary'].mean()
     return "In 'YA' region the average salary of a 'Developer' is " + str(mean_salary) + "\n"
 
 
 # Program directory is only in CA region
-def filter_prog_dir_CA(df_filtered_cols):
-    CA_prog_dir = df_filtered_cols[
+# In this method we are filtering Program Directory in CA region
+def filter_prog_dir_ca(df_filtered_cols):
+    ca_prog_dir = df_filtered_cols[
         (df_filtered_cols['jobTitleName'] == 'Program Directory') & (df_filtered_cols['region'] == 'CA')]
-    return CA_prog_dir
+    return ca_prog_dir
 
 
-def avg_sal_prog_dir_CA(CA_prog_dir):
-    mean_salary = CA_prog_dir['salary'].mean()
+# In this method we are calculating the average salary of a Program Directory in CA region by taking a mean of the salary column
+def avg_sal_prog_dir_ca(ca_prog_dir):
+    mean_salary = ca_prog_dir['salary'].mean()
     return "In 'CA' region the average salary of a 'Program Directory' is " + str(mean_salary) + "\n"
 
 
 # Product owner CA
-def filter_prod_own_CA(df_filtered_cols):
-    CA_prod_own = df_filtered_cols[
+# In this method we are filtering Product owner in CA region
+def filter_prod_own_ca(df_filtered_cols):
+    ca_prod_own = df_filtered_cols[
         (df_filtered_cols['jobTitleName'] == 'ProductOwner') & (df_filtered_cols['region'] == 'CA')]
-    return CA_prod_own
+    return ca_prod_own
 
 
-def avg_sal_prod_own_CA(CA_prod_own):
-    mean_salary = CA_prod_own['salary'].mean()
+# In this method we are calculating the average salary of a Product owner in CA region by taking a mean of the salary column
+def avg_sal_prod_own_ca(ca_prod_own):
+    mean_salary = ca_prod_own['salary'].mean()
     return "In 'CA' region the average salary of a 'ProductOwner' is " + str(mean_salary) + "\n"
 
 
 # product owner SA
-def filter_prod_own_SA(df_filtered_cols):
-    SA_prod_own = df_filtered_cols[
+# In this method we are filtering Product owner in SA region
+def filter_prod_own_sa(df_filtered_cols):
+    sa_prod_own = df_filtered_cols[
         (df_filtered_cols['jobTitleName'] == 'ProductOwner') & (df_filtered_cols['region'] == 'SA')]
-    return SA_prod_own
+    return sa_prod_own
 
 
-def avg_sal_prod_own_SA(SA_prod_own):
-    mean_salary = SA_prod_own['salary'].mean()
+# In this method we are calculating the average salary of a Product owner in SA region by taking a mean of the salary column
+def avg_sal_prod_own_sa(sa_prod_own):
+    mean_salary = sa_prod_own['salary'].mean()
     return "In 'SA' region the average salary of a 'ProductOwner' is " + str(mean_salary) + "\n"
 
 
 # product owner YA
-def filter_prod_own_YA(df_filtered_cols):
-    YA_prod_own = df_filtered_cols[
+# In this method we are filtering Product owner in YA region
+def filter_prod_own_ya(df_filtered_cols):
+    ya_prod_own = df_filtered_cols[
         (df_filtered_cols['jobTitleName'] == 'ProductOwner') & (df_filtered_cols['region'] == 'YA')]
-    return YA_prod_own
+    return ya_prod_own
 
 
-def avg_sal_prod_own_YA(YA_prod_own):
-    mean_salary = YA_prod_own['salary'].mean()
+# In this method we are calculating the average salary of a Product owner in YA region by taking a mean of the salary column
+def avg_sal_prod_own_ya(ya_prod_own):
+    mean_salary = ya_prod_own['salary'].mean()
     return "In 'YA' region the average salary of a 'ProductOwner' is " + str(mean_salary) + "\n"
 
 
+# Here we are printing all the data to a output file (average salary of Developer, Program Directory and Product Owner in particular region)
 def write_to_out():
-    outfile = open('../resources//output.txt', 'w')
-    outfile.write(avg_sal_dev_CA(filter_dev_CA(filter_necessary_cols(read_json()))))
-    outfile.write(avg_sal_dev_FA(filter_dev_FA(filter_necessary_cols(read_json()))))
-    outfile.write(avg_sal_dev_YA(filter_dev_YA(filter_necessary_cols(read_json()))))
-    outfile.write(avg_sal_prog_dir_CA(filter_prog_dir_CA(filter_necessary_cols(read_json()))))
-    outfile.write(avg_sal_prod_own_CA(filter_prod_own_CA(filter_necessary_cols(read_json()))))
-    outfile.write(avg_sal_prod_own_SA(filter_prod_own_SA(filter_necessary_cols(read_json()))))
-    outfile.write(avg_sal_prod_own_YA(filter_prod_own_YA(filter_necessary_cols(read_json()))))
+    outfile = open('../outputs//output.txt', 'w')
+    outfile.write(avg_sal_dev_ca(filter_dev_ca(filter_necessary_cols(read_json()))))
+    outfile.write(avg_sal_dev_fa(filter_dev_fa(filter_necessary_cols(read_json()))))
+    outfile.write(avg_sal_dev_ya(filter_dev_ya(filter_necessary_cols(read_json()))))
+    outfile.write(avg_sal_prog_dir_ca(filter_prog_dir_ca(filter_necessary_cols(read_json()))))
+    outfile.write(avg_sal_prod_own_ca(filter_prod_own_ca(filter_necessary_cols(read_json()))))
+    outfile.write(avg_sal_prod_own_sa(filter_prod_own_sa(filter_necessary_cols(read_json()))))
+    outfile.write(avg_sal_prod_own_ya(filter_prod_own_ya(filter_necessary_cols(read_json()))))
 
 
 if __name__ == "__main__":
--- tests/test_avg_salary.py
+++ tests/test_avg_salary.py
@@ -51,31 +51,31 @@ def test_filter_necessary_cols(original_df, filtered_df):
 
 
 # In this test case we are checking whether we have filtered developer's from CA region
-def test_filter_dev_CA(filtered_df):
-    actual = filter_dev_CA(filtered_df)
+def test_filter_dev_ca(filtered_df):
+    actual = filter_dev_ca(filtered_df)
 
-    dev_CA_df = {
+    dev_ca_df = {
         "jobTitleName": ['Developer', 'Developer'],
         "region": ['CA', 'CA'],
         "salary": ['20000', '30000']
     }
 
-    expected = pd.DataFrame(dev_CA_df, columns=['jobTitleName', 'region', 'salary'])
+    expected = pd.DataFrame(dev_ca_df, columns=['jobTitleName', 'region', 'salary'])
     expected['salary'] = expected['salary'].astype('int')
     pd.testing.assert_frame_equal(actual, expected)
 
 
 # In this test case we are checking whether we are calculating and printing the correct average salary of developer's from CA region
-def test_avg_sal_CA():
-    dev_CA_df = {
+def test_dev_avg_sal_ca():
+    dev_ca_df = {
         "jobTitleName": ['Developer', 'Developer'],
         "region": ['CA', 'CA'],
         "salary": ['20000', '30000']
     }
-    test_df = pd.DataFrame(dev_CA_df, columns=['jobTitleName', 'region', 'salary'])
+    test_df = pd.DataFrame(dev_ca_df, columns=['jobTitleName', 'region', 'salary'])
     test_df['salary'] = test_df['salary'].astype('int')
 
-    actual = avg_sal_dev_CA(test_df)
+    actual = avg_sal_dev_ca(test_df)
 
     expected = "In 'CA' region the average salary of a 'Developer' is 25000.0\n"
 
@@ -83,35 +83,31 @@ def test_avg_sal_CA():
 
 
 # In this test case we are checking whether we have filtered developer's from FA region
-def test_filter_dev_FA(filtered_df):
-    actual = filter_dev_FA(filtered_df)
+def test_filter_dev_fa(filtered_df):
+    actual = filter_dev_fa(filtered_df)
 
-    dev_FA_df = {
+    dev_fa_df = {
         "jobTitleName": ['Developer'],
         "region": ['FA'],
         "salary": ['10000']
     }
 
-    expected = pd.DataFrame(dev_FA_df, columns=['jobTitleName', 'region', 'salary'])
+    expected = pd.DataFrame(dev_fa_df, columns=['jobTitleName', 'region', 'salary'])
     expected['salary'] = expected['salary'].astype('int')
     pd.testing.assert_frame_equal(actual.reset_index(drop=True), expected.reset_index(drop=True))
-    # print("----Actual----")
-    # print(actual)
-    # print("----Expected----")
-    # print(expected)
 
 
 # In this test case we are checking whether we are calculating and printing the correct average salary of developer's from FA region
-def test_avg_sal_FA():
-    dev_FA_df = {
+def test_dev_avg_sal_fa():
+    dev_fa_df = {
         "jobTitleName": ['Developer'],
         "region": ['FA'],
         "salary": ['10000']
     }
-    test_df = pd.DataFrame(dev_FA_df, columns=['jobTitleName', 'region', 'salary'])
+    test_df = pd.DataFrame(dev_fa_df, columns=['jobTitleName', 'region', 'salary'])
     test_df['salary'] = test_df['salary'].astype('int')
 
-    actual = avg_sal_dev_FA(test_df)
+    actual = avg_sal_dev_fa(test_df)
 
     expected = "In 'FA' region the average salary of a 'Developer' is 10000.0\n"
 
@@ -119,32 +115,166 @@ def test_avg_sal_FA():
 
 
 # In this test case we are checking whether we have filtered developer's from YA region
-def test_filter_dev_YA(filtered_df):
-    actual = filter_dev_YA(filtered_df)
+def test_filter_dev_ya(filtered_df):
+    actual = filter_dev_ya(filtered_df)
 
-    dev_YA_df = {
+    dev_ya_df = {
         "jobTitleName": ['Developer'],
         "region": ['YA'],
         "salary": ['20000']
     }
 
-    expected = pd.DataFrame(dev_YA_df, columns=['jobTitleName', 'region', 'salary'])
+    expected = pd.DataFrame(dev_ya_df, columns=['jobTitleName', 'region', 'salary'])
     expected['salary'] = expected['salary'].astype('int')
     pd.testing.assert_frame_equal(actual.reset_index(drop=True), expected.reset_index(drop=True))
 
 
 # In this test case we are checking whether we are calculating and printing the correct average salary of developer's from YA region
-def test_avg_sal_YA():
-    dev_YA_df = {
+def test_dev_avg_sal_ya():
+    dev_ya_df = {
         "jobTitleName": ['Developer'],
         "region": ['YA'],
         "salary": ['20000']
     }
-    test_df = pd.DataFrame(dev_YA_df, columns=['jobTitleName', 'region', 'salary'])
+    test_df = pd.DataFrame(dev_ya_df, columns=['jobTitleName', 'region', 'salary'])
     test_df['salary'] = test_df['salary'].astype('int')
 
-    actual = avg_sal_dev_YA(test_df)
+    actual = avg_sal_dev_ya(test_df)
 
     expected = "In 'YA' region the average salary of a 'Developer' is 20000.0\n"
 
     assert actual == expected
+
+
+# In this test case we are checking whether we have filtered program directory from CA region, also program directory is only in CA region
+def test_filter_prog_dir_ca(filtered_df):
+    actual = filter_prog_dir_ca(filtered_df)
+
+    prog_dir_ca_df = {
+        "jobTitleName": ['Program Directory'],
+        "region": ['CA'],
+        "salary": ['15000']
+    }
+
+    expected = pd.DataFrame(prog_dir_ca_df, columns=['jobTitleName', 'region', 'salary'])
+    expected['salary'] = expected['salary'].astype('int')
+    pd.testing.assert_frame_equal(actual.reset_index(drop=True), expected.reset_index(drop=True))
+
+
+# In this test case we are checking whether we are calculating and printing the correct average salary of program directory from CA region
+def test_prog_dir_avg_sal_ca():
+    prog_dir_ca_df = {
+        "jobTitleName": ['Program Directory'],
+        "region": ['CA'],
+        "salary": ['15000']
+    }
+    test_df = pd.DataFrame(prog_dir_ca_df, columns=['jobTitleName', 'region', 'salary'])
+    test_df['salary'] = test_df['salary'].astype('int')
+
+    actual = avg_sal_prog_dir_ca(test_df)
+
+    expected = "In 'CA' region the average salary of a 'Program Directory' is 15000.0\n"
+
+    assert actual == expected
+
+
+# In this test case we are checking whether we have filtered productOwner's from CA region
+def test_filter_prod_own_ca(filtered_df):
+    actual = filter_prod_own_ca(filtered_df)
+
+    prod_own_ca_df = {
+        "jobTitleName": ['ProductOwner'],
+        "region": ['CA'],
+        "salary": ['2000']
+    }
+
+    expected = pd.DataFrame(prod_own_ca_df, columns=['jobTitleName', 'region', 'salary'])
+    expected['salary'] = expected['salary'].astype('int')
+    pd.testing.assert_frame_equal(actual.reset_index(drop=True), expected.reset_index(drop=True))
+
+
+# In this test case we are checking whether we are calculating and printing the correct average salary of program directory from CA region
+def test_prod_own_avg_sal_ca():
+    prod_own_ca_df = {
+        "jobTitleName": ['ProductOwner'],
+        "region": ['CA'],
+        "salary": ['2000']
+    }
+    test_df = pd.DataFrame(prod_own_ca_df, columns=['jobTitleName', 'region', 'salary'])
+    test_df['salary'] = test_df['salary'].astype('int')
+
+    actual = avg_sal_prod_own_ca(test_df)
+
+    expected = "In 'CA' region the average salary of a 'ProductOwner' is 2000.0\n"
+
+    assert actual == expected
+
+
+# In this test case we are checking whether we have filtered productOwner's from SA region
+def test_filter_prod_own_sa(filtered_df):
+    actual = filter_prod_own_sa(filtered_df)
+
+    prod_own_sa_df = {
+        "jobTitleName": ['ProductOwner', 'ProductOwner', 'ProductOwner'],
+        "region": ['SA', 'SA', 'SA'],
+        "salary": ['45000', '25000', '25000']
+    }
+
+    expected = pd.DataFrame(prod_own_sa_df, columns=['jobTitleName', 'region', 'salary'])
+    expected['salary'] = expected['salary'].astype('int')
+    pd.testing.assert_frame_equal(actual.reset_index(drop=True), expected.reset_index(drop=True))
+
+
+# In this test case we are checking whether we are calculating and printing the correct average salary of program directory from SA region
+def test_prod_own_avg_sal_sa():
+    prod_own_sa_df = {
+        "jobTitleName": ['ProductOwner', 'ProductOwner', 'ProductOwner'],
+        "region": ['SA', 'SA', 'SA'],
+        "salary": ['45000', '25000', '25000']
+    }
+    test_df = pd.DataFrame(prod_own_sa_df, columns=['jobTitleName', 'region', 'salary'])
+    test_df['salary'] = test_df['salary'].astype('int')
+
+    actual = avg_sal_prod_own_sa(test_df)
+
+    expected = "In 'SA' region the average salary of a 'ProductOwner' is 31666.666666666668\n"
+
+    assert actual == expected
+
+
+# In this test case we are checking whether we have filtered productOwner's from SA region
+def test_filter_prod_own_ya(filtered_df):
+    actual = filter_prod_own_ya(filtered_df)
+
+    prod_own_ya_df = {
+        "jobTitleName": ['ProductOwner'],
+        "region": ['YA'],
+        "salary": ['17000']
+    }
+
+    expected = pd.DataFrame(prod_own_ya_df, columns=['jobTitleName', 'region', 'salary'])
+    expected['salary'] = expected['salary'].astype('int')
+    pd.testing.assert_frame_equal(actual.reset_index(drop=True), expected.reset_index(drop=True))
+
+
+# In this test case we are checking whether we are calculating and printing the correct average salary of program directory from YA region
+def test_prod_own_avg_sal_ya():
+    prod_own_ya_df = {
+        "jobTitleName": ['ProductOwner'],
+        "region": ['YA'],
+        "salary": ['17000']
+    }
+    test_df = pd.DataFrame(prod_own_ya_df, columns=['jobTitleName', 'region', 'salary'])
+    test_df['salary'] = test_df['salary'].astype('int')
+
+    actual = avg_sal_prod_own_ya(test_df)
+
+    expected = "In 'YA' region the average salary of a 'ProductOwner' is 17000.0\n"
+
+    assert actual == expected
+
+
+# def test_writetofile(tmpdir):
+#     file = tmpdir.join('../outputs//output.txt')
+#     write_to_out()
+#     assert file.read() == "In 'CA' region the average salary of a 'Developer' is 25000.0"
